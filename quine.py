23375cf5464f29d0983aadf62935cc167c328142
owe005 <owe05@hotmail.no>
2023-09-20 22:32:56 +0200

    quine

---
+++ quine.py
@@ -0,0 +1,12 @@
+a= "print('a = ' + chr(34) + a + chr(34) + chr(10) + a)"
+
+t = ['Ole Westby']
+print("t = ['Ole Westby']")
+
+print('a = ' + chr(34) + a + chr(34) + chr(10) + a)
+
+b = 'b = %r\nprint(b %% b)'
+
+print(b % b)
+
+c= 'c = {!r};print(c.format(c))';print(c.format(c))
